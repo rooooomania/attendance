a31fb8c323ea6400429df78dccc26d5284e930e2
MORIYA-SEI <rooooomania@icloud.com>
2015-07-25 14:26:53 +0900

    build tool を stack に変更

--- Foundation.hs
+++ Foundation.hs
@@ -168,6 +168,7 @@ permissionRequiredFor RootR _ = []
 permissionRequiredFor (CancelR _ _) _ = []
 permissionRequiredFor UsersR _ = [ViewUsers]
 permissionRequiredFor (ApproveR _ _) _ = []
+permissionRequiredFor (User _) _ = []
 
 
 
